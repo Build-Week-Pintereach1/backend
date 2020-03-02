c0327dcdaddb48d222d952db54cb38af32ff2942
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-03-02 06:22:54 -0800

    Restrict user route

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -25,7 +25,25 @@ function validateUserInfo(req, res, next) {
   }
 }
 
+function restricted(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    jwt.verify(authorization, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid credentials.' });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'Please login to access.' });
+  }
+}
+
 module.exports = {
   generateToken,
-  validateUserInfo
+  validateUserInfo,
+  restricted
 };
