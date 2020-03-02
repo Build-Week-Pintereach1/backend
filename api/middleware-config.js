3f3c291ae004fca9f3cd260bc27b7b2eab5f176c
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-03-02 01:05:28 -0800

    Add logger middleware

--- api/middleware-config.js
+++ api/middleware-config.js
@@ -2,8 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const logger = (req, res, next) => {
+  console.log(`[${new Date().toUTCString()}] ${req.method} to ${req.originalUrl}`);
+  next();
+};
+
 module.exports = server => {
   server.use(helmet());
   server.use(express.json());
   server.use(cors());
+  server.use('/', logger);
 };
--- api/server.js
+++ api/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+
+const configureMiddleware = require('./middleware-config.js');
+
 const server = express();
+configureMiddleware(server);
 
 server.get('/', (req, res) => {
   res.json({ message: "Pintereach is live" });
