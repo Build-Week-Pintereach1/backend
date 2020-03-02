180c60fb040295dd591823bb08da1cac16064f19
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-03-02 02:03:44 -0800

    Configure knex

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
+  testing: {
+    client: 'postgresql',
+    connection: process.env.TEST_DB_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    }
+  }
+};
